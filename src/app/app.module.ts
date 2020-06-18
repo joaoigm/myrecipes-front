940145f1f5e458fe74103b0527025e50e47be378
João Igor <joao.melo@esx.com.br>
2020-06-17 23:10:10 -0300

    adicionando os componentes dos recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesDetailComponent } from './recipes-detail/recipes-detail.component';
+import { RecipesFormComponent } from './recipes-form/recipes-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    RecipesDetailComponent,
+    RecipesFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes-form/recipes-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipesFormComponent } from './recipes-form.component';
+
+describe('RecipesFormComponent', () => {
+  let component: RecipesFormComponent;
+  let fixture: ComponentFixture<RecipesFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RecipesFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecipesFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
